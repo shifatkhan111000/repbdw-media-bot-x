03de1b65a09d136b70cb3c3198db0e9decdab5a4
shifatkhan111000 <shifatappleapple@gmail.com>
2026-07-06 01:41:21 +0600

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -1,13 +1,21 @@
 from pyrogram import Client, filters
+from database import movies
 
-@Client.on_message(filters.command("search"))
+@Client.on_message(filters.text)
 async def search_movie(client, message):
-    if len(message.command) < 2:
-        return await message.reply_text(
-            "Usage: /search movie_name"
-        )
 
-    movie = " ".join(message.command[1:])
+    movie = await movies.find_one({
+        "title": message.text.lower()
+    })
+
+    if not movie:
+        return
+
     await message.reply_text(
-        f"Searching for: {movie}"
+        f"🎬 {movie['title'].title()} ({movie['year']})"
+    )
+
+    await client.send_video(
+        chat_id=message.chat.id,
+        video=movie["file_id"]
     )
