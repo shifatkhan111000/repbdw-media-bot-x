1e40ff13c54dbabc8738fd41e510a4fd02650539
shifatkhan111000 <shifatappleapple@gmail.com>
2026-07-06 01:30:44 +0600

    Add movie search functionality to the bot

--- plugins/search.py
+++ plugins/search.py
@@ -1,1 +1,21 @@
+from pyrogram import Client, filters
+from database import movies
 
+@Client.on_message(filters.text)
+async def search_movie(client, message):
+
+    movie = await movies.find_one({
+        "title": message.text.lower()
+    })
+
+    if not movie:
+        return
+
+    await message.reply_text(
+        f"🎬 {movie['title'].title()} ({movie['year']})"
+    )
+
+    await client.send_video(
+        chat_id=message.chat.id,
+        video=movie["file_id"]
+    )
